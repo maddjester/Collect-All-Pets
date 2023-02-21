f611ba888014eddd55945e9571f810f2666aaf19
maddjester <65257955+maddjester@users.noreply.github.com>
2023-02-21 01:48:23 -0600

    Updated

--- Collect All Pets!.lua
+++ Collect All Pets!.lua
@@ -1,4 +1,4 @@
-print("loading --")
+if not game:IsLoaded() then print("loading --"); game.Loaded:Wait() end;
 local ws = game:GetService("Workspace")
 local lp = game:GetService("Players").LocalPlayer
 
@@ -13,6 +13,36 @@ local Camera = ws.Camera
 local Hatcher = lp.PlayerGui.ScreenGui.Hatcher
 local Crystals = ws.Crystals
 
+local TweenService  = game:GetService("TweenService")
+local noclipE = true
+local antifall = true
+
+local function noclip()
+    for i, v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+        if v:IsA("BasePart") and v.CanCollide == true then
+            v.CanCollide = false
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+        end
+    end
+end
+
+local function moveto(obj, speed)
+    local info = TweenInfo.new(((game.Players.LocalPlayer.Character.HumanoidRootPart.Position - obj.Position).Magnitude)/ speed,Enum.EasingStyle.Linear)
+    local tween = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, info, {CFrame = obj})
+
+    if not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("BodyVelocity") then
+        antifall = Instance.new("BodyVelocity", game.Players.LocalPlayer.Character.HumanoidRootPart)
+        antifall.Velocity = Vector3.new(0,0,0)
+        noclipE = game:GetService("RunService").Stepped:Connect(noclip)
+        tween:Play()
+    end
+        
+    tween.Completed:Connect(function()
+        antifall:Destroy()
+        noclipE:Disconnect()
+    end)
+end
+
 local function keyPress(str, secnds)
     local secnds = secnds or 0.1
     if str then
@@ -23,26 +53,6 @@ local function keyPress(str, secnds)
     end
 end
 
-local function tweenTo(pos)
-    if pos then
-        local dist = (pos - part.Position)
-        local timeDelay = (dist / noid.WalkSpeed)
-
-        if noid.Sit then noid.Sit = not noid.Sit end;
-        local tween_s = game:GetService("TweenService")
-        local tweeninfo = TweenInfo.new(timeDelay, Enum.EasingStyle.Linear, Enum.EasingDirection.In, 0, false, 0)
-        local newPos = pos + Vector3.new(0, 6, -1)
-        local cf = CFrame.new(newPos)
-        local animation = tween_s:Create(part, tweeninfo, {CFrame = cf})
-        keypress(0x57)
-        animation:Play()
-        task.wait(timeDelay)
-        keyrelease(0x57)
-        animation:Cancel()
-        animation:Destroy()
-    end
-end
-
 local function buyEgg(index)
     local Event = game:GetService("ReplicatedStorage").Remotes.BuyEgg
     Event:FireServer(index)
@@ -50,6 +60,11 @@ end
 
 local function claimReward()
     local Event = game:GetService("ReplicatedStorage").Remotes.ClaimQuestReward
+    Event:FireServer()
+end
+
+local function claimDailyEgg()
+    local Event = game:GetService("ReplicatedStorage").Remotes.ClaimDailyEgg
     Event:FireServer()
 end
 
@@ -72,21 +87,22 @@ end
 
 local function getSuperArea()
     local Super = Crystals.Super
-    local c = nil
+    local UnlockedArea = lp.UnlockedArea
     for _, v in pairs(Super:GetChildren()) do
         -- print(i, v)
         if v.Time.Value > 0 then
-            c = v.Area.Value
-            return c
+            if UnlockedArea.Value <= v.Area.Value then
+                return v.Area.Value
+            end
         end
     end
+    return nil
 end
 
 local function getProgress()
     local g, p = 0, 0
     if getSuperArea() then
         local superFrame = lp.PlayerGui.ScreenGui.Main.Top.SuperCrystalFrame
-
     end
     if lp.QuestGoal and lp.QuestProgress then
         g, p = lp.QuestGoal.Value, lp.QuestProgress.Value
@@ -103,22 +119,22 @@ local function getPetNames()
     return p
 end
 
-local function closeHatcher()
-    if Camera.CameraType ~= "Custom" and Hatcher.Visible then
-        keyPress("E", 0.2)
-    end
-end
-
 local function checkExotics(questArea)
     local areaName = Areas[questArea].Name
     local areaCrystals = Crystals[areaName]
-    for i, v in pairs(areaCrystals:GetDescendants()) do
+    for _, v in pairs(areaCrystals:GetDescendants()) do
         -- print(i, v)
         if v.Name == "Base" and v.Position then
             print("Found Exotic Crystal!")
-            tweenTo(v.Position)
+            pcall(function()
+                moveto(v.CFrame + Vector3.new(0,6,0), 50)
+            end)
+            local mag = noid.MoveDirection.Magnitude
+            if mag > 0 then
+                repeat task.wait() until mag == 0
+            end
             keyPress("R")
-            task.wait(5)
+            task.wait(10)
         end
     end
 end
@@ -127,23 +143,29 @@ local function autoQuest()
     if getgenv().QUEST then
         local questCompleted = 0
 
-        while task.wait(5) do
+        while task.wait(1) do
             if not getgenv().QUEST then break end;
-            if not part then repeat task.wait(1) until part end
-            closeHatcher()
+            if not part then repeat task.wait(1) until part end;
+            if noid.Sit then noid.Sit = not noid.Sit end;
 
+            if Camera.CameraType ~= "Custom" or Hatcher.Visible then
+                keyPress("E", 0.2)
+            end
             local inArea, questArea = getAreas() -- Get updated values for current area and quest area
-            if getSuperArea() ~= nil then
+            
+            if getgenv().SUPER and getSuperArea() ~= nil then
+
                 questArea = getSuperArea()
             end
 
             local nextArea = nextArea(inArea, questArea) -- Figure out which direction to travel positon +1, -1
             -- print("Next Area =", nextArea)
+
             if nextArea == 0 and questArea == 0 then -- Set next area to first area to avoid timeout if questArea is 0
                 nextArea = 1
             end
+
             if inArea == questArea then -- Farm until questArea changes
-                checkExotics(questArea)
                 local totProg, myProg = getProgress() -- Get Updated values for progress
                 if myProg == totProg then -- Claim reward if quest is complete
                     claimReward()
@@ -153,11 +175,14 @@ local function autoQuest()
                         keyPress("R")
                     end
                 end
-                print("Current Quest Progress/Goal --", myProg.."/"..totProg)
+                if getgenv().EXOTIC then
+                    checkExotics(questArea)
+                end
+                print("Quest Progress/Goal --", myProg.."/"..totProg)
             elseif inArea ~= nextArea and nextArea > 0 then -- Move to next area if not in the questArea
-                local pos = Areas[nextArea].Position
-                print("Moving to:", areas[nextArea])
-                tweenTo(pos)
+                pcall(function()
+                    moveto(Areas[nextArea].CFrame + Vector3.new(0,6,0), 50)
+                end)
             end
             print("Current Area:", areas[inArea], "Target Area:", areas[questArea])
         end
@@ -165,24 +190,37 @@ local function autoQuest()
 end
 
 -- GUI Configuration
-
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
 local w = library:CreateWindow("Collect All Pets!")
 local b = w:CreateFolder("Questing")
 local c = w:CreateFolder("Pets & Eggs")
 local d = w:CreateFolder("Badges")
-local z = w:CreateFolder("Gui")
+local z = w:CreateFolder("Server/Gui")
 
 getgenv().QUEST = false
 
-b:Toggle("Auto Quest", function(bool)
+b:Toggle("Auto-Quest Complete", function(bool)
     getgenv().QUEST = bool
-    print("Auto Quest:", bool)
+    print("Auto-Quest Complete:", bool)
     if bool then
         task.spawn(function()
             autoQuest()
         end)
     end
+end)
+
+getgenv().SUPER = false
+
+b:Toggle("Go to Super Crystals", function(bool)
+    getgenv().SUPER = bool
+    print("Auto-Super Crystal:", bool)
+end)
+
+getgenv().EXOTIC = false
+
+b:Toggle("Go to Exotic Crystals", function(bool)
+    getgenv().EXOTIC = bool
+    print("Auto-Exotic Crystal:", bool)
 end)
 
 local function tpTo(place)
@@ -203,22 +241,38 @@ b:Dropdown("Teleport to Area", areas, false, function(place)
     tpTo(place)
 end)
 
+c:Toggle("Auto-Claim Daily Egg", function(bool)
+    getgenv().QUEST = bool
+    print("Auto-Claim Daily Egg:", bool)
+    if bool then
+        task.spawn(function()
+            claimDailyEgg()
+        end)
+    end
+end)
+
 getgenv().ALWAYSEQUIPBEST = false
+
 c:Toggle("Always Equip Best Pets", function(bool)
     getgenv().ALWAYSEQUIPBEST = bool
+    print("Always Equip Best Pets:", bool)
 end)
+
 c:Button("Equip Best Pets",function()
     keyPress("R")
 end)
 
-c:Dropdown("Select type",{"Common","Unommon","Rare","Epic","Legendary"}, true, function(eggType)
+c:Dropdown("Select type",{"Common","Uncommon","Rare","Epic","Legendary"}, true, function(eggType)
     getgenv().EGGTYPE = eggType
+    print("Selected Type:", eggType)
 end)
+
 getgenv().EGGTYPE = nil
-c:Button("Buy Egg",function()
+
+c:Button("Buy Egg", function()
     local eggNum = {
         ["Common"] = 1,
-        ["Unommon"] = 2,
+        ["Uncommon"] = 2,
         ["Rare"] = 3,
         ["Epic"] = 4,
         ["Legendary"] = 5
@@ -234,10 +288,10 @@ c:Button("Buy Egg",function()
     local playerGold = lp.Gold.Value
     if type then
         if playerGold >= eggPrice[type] then
-            buyEgg(eggNum[type])
-            task.wait(3)
-            closeHatcher()
-            print("Bought a", type, "egg!")
+            task.spawn(function()
+                buyEgg(eggNum[type])
+                print("Bought a", type, "egg!")
+            end)
         else
             print("Not enough gold to buy", type, "egg.")
         end
@@ -271,6 +325,7 @@ d:Toggle("Get Badges", function(bool)
     else
         cDestroyed.Value = oldVal
     end
+    print("Get Badges:", bool)
 end)
 
 z:Label("Right Ctrl = Hide/Show Gui",{
@@ -278,5 +333,27 @@ z:Label("Right Ctrl = Hide/Show Gui",{
     TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
     BgColor = Color3.fromRGB(69,69,69); -- Self Explaining 
 })
+
+local function redeemCodes()
+    local codes = loadstring(game:HttpGet())
+    for _, code in pairs(codes) do
+        local A_1 = code
+        local Event = game:GetService("ReplicatedStorage").Remotes.RedeemCode
+        Event:FireServer(A_1)
+    end
+    print("Codes redeemed!")
+end
+
+z:Button("Redeem Codes", function()
+    redeemCodes()
+end)
+z:Button("Server Hop", function()
+    local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
+    if game and module then
+        print("Hopping...")
+        module:Teleport(game.PlaceId)
+    end
+end)
+
 z:DestroyGui()
 print("loaded --")
